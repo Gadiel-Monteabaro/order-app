8a89e27a4dd97ac998fc081bb7e7b6b4ceb039f8
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2024-11-30 14:09:11 -0300

    Modificar css

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -15,6 +15,9 @@ export default function Product({ item, addToOrder }: ItemProductProps) {
       <img src={`${item.img}.jpg`} alt="imagen del producto" />
       <p>{item.name}</p>
       <p>${item.price}</p>
+      <div className="product-modal">
+        <h2>{item.name}</h2>
+      </div>
     </button>
   );
 }
